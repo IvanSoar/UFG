3c27dbe8bc0018180fec3b2a44235a5d7be4ab7c
Ivan Soares <programadorivansoares@gmail.com>
2019-11-22 17:50:01 -0300

    implementado busca, inserção e exclusão, entre outros

--- AED1/lista6/main1.c
+++ AED1/lista6/main1.c
@@ -8,9 +8,11 @@ typedef struct tipoPersonalizado conjunto;
 int criaConjunto(conjunto **);
 int conjuntoVazio(conjunto *);
 int insereElementoConjunto(int, conjunto **);
-int verificaCapacidade(conjunto **);
+void verificaCapacidade(conjunto **);
 int pertenceConjunto(int , conjunto *);
 void imprimeElementosConjunto(conjunto *);
+int excluiElementoConjunto(int, conjunto **);
+int tamanhoConjunto(conjunto *);
 
 /**/
 
@@ -29,7 +31,6 @@ int criaConjunto(conjunto ** pConjunto)
         (*pConjunto)->contagemElementos = 0;
         (*pConjunto)->capacidade = CAPACIDADE_INICIAL_CONJUNTO;
         (*pConjunto)->elementos = (int *)malloc(sizeof(int) * (*pConjunto)->capacidade);
-        printf("%p\n", (*pConjunto));
         return 1;
     }
     else
@@ -38,13 +39,15 @@ int criaConjunto(conjunto ** pConjunto)
     }
 }
 
-int conjuntoVazio(conjunto * pConjunto){
+int conjuntoVazio(conjunto * pConjunto)
+{
     return pConjunto->contagemElementos == 0 ? 1 : 0;
 }
 
-int insereElementoConjunto(int elemento, conjunto ** pConjunto){
+int insereElementoConjunto(int elemento, conjunto ** pConjunto)
+{
     verificaCapacidade(pConjunto);
-    if(pertenceConjunto(elemento, *pConjunto)){
+    if(pertenceConjunto(elemento, (*pConjunto))){
         return 0;
     } else {
         (*pConjunto)->elementos[(*pConjunto)->contagemElementos] = elemento;
@@ -53,43 +56,102 @@ int insereElementoConjunto(int elemento, conjunto ** pConjunto){
     }
 }
 
-int verificaCapacidade(conjunto ** pConjunto){
-    return 1;
+void verificaCapacidade(conjunto ** pConjunto)
+{
+    if((*pConjunto)->contagemElementos == (*pConjunto)->capacidade)
+    {
+        (*pConjunto)->capacidade *= 2;
+        (*pConjunto)->elementos = (int *) realloc((*pConjunto)->elementos, sizeof(int) * (*pConjunto)->capacidade);
+    }
 }
 
-int pertenceConjunto(int elemento, conjunto * pConjunto){
+int pertenceConjunto(int elemento, conjunto * pConjunto)
+{
+    int i;
+    for (i = 0; i < pConjunto->contagemElementos; i++)
+    {
+        if(pConjunto->elementos[i] == elemento){
+            return 1;
+        }
+    }
     return 0;
 }
 
-void imprimeElementosConjunto(conjunto * pConjunto){
+void imprimeElementosConjunto(conjunto * pConjunto)
+{
     int i;
-    printf("{ ");
-    for (i = 0; i < pConjunto->contagemElementos; i++)
+    if (pConjunto->contagemElementos == 0)
     {
-        printf("%d", pConjunto->elementos[i]);
-        printf("%s", i < pConjunto->contagemElementos - 1 ? ", " : "");
+        printf("Conjunto vazio.\n");
     }
-    printf(" }");
+    else
+    {
+        printf("{");
+        for (i = 0; i < pConjunto->contagemElementos; i++)
+        {
+            printf("%d", pConjunto->elementos[i]);
+            printf("%s", i < pConjunto->contagemElementos - 1 ? ", " : "");
+        }
+        printf("}\n");
+    }
+}
+
+int excluiElementoConjunto(int elemento, conjunto ** pConjunto)
+{
+    int i, indice;
+    if(pertenceConjunto(elemento, (*pConjunto))){
+        for (i = 0; i < (*pConjunto)->contagemElementos; i++)
+        {
+            if((*pConjunto)->elementos[i] == elemento){
+                indice = i;
+                break;
+            }
+        }
+
+        for (i = indice; i < (*pConjunto)->contagemElementos - 1; i++){
+            (*pConjunto)->elementos[i] = (*pConjunto)->elementos[i + 1];
+        }
+
+        (*pConjunto)->contagemElementos--;
+    } else {
+        printf("Solicitacao de exclusao sem sucesso. Elemento nao encontrado.\n");
+        return 0;
+    }
+}
+
+int tamanhoConjunto(conjunto * pConjunto)
+{
+    return pConjunto->contagemElementos;
 }
 
 /**/
 
 int main()
 {
-    conjunto * estrutura;
-    criaConjunto(&estrutura);
+    conjunto * C;
+    criaConjunto(&C);
 
-    printf("%p\n", estrutura);
+    insereElementoConjunto(5, &C);
+    insereElementoConjunto(10, &C);
+    insereElementoConjunto(15, &C);
+    insereElementoConjunto(16, &C);
+    insereElementoConjunto(26, &C);
 
-    insereElementoConjunto(5, &estrutura);
-    insereElementoConjunto(10, &estrutura);
-    insereElementoConjunto(15, &estrutura);
-    insereElementoConjunto(16, &estrutura);
+    insereElementoConjunto(5, &C);
+    insereElementoConjunto(10, &C);
+    insereElementoConjunto(15, &C);
+    insereElementoConjunto(16, &C);
+    insereElementoConjunto(26, &C);
 
-    if(conjuntoVazio(estrutura)){
-        printf("Conjunto vazio.\n");
-    } else {
-        printf("Conjunto nao vazio.\n");
-        imprimeElementosConjunto(estrutura);
-    }
+    imprimeElementosConjunto(C);
+
+    excluiElementoConjunto(5, &C);
+    excluiElementoConjunto(10, &C);
+    excluiElementoConjunto(15, &C);
+    excluiElementoConjunto(16, &C);
+    excluiElementoConjunto(26, &C);
+
+    excluiElementoConjunto(260, &C);
+
+    imprimeElementosConjunto(C);
 }
